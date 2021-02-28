93272f30360a04747670155f312ca1eb7c58a09e
Malte <wallenmalte@gmail.com>
2021-02-28 13:37:26 +0100

    invisible text

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 var gameWrapper = document.getElementsByClassName("GameItemWrap");
-//alert("start")
 for(var i = 0; i < gameWrapper.length; i++){
 	var championImage = gameWrapper[i].getElementsByClassName("Image16 __sprite tip");
 	var summonerName = gameWrapper[i].getElementsByClassName("SummonerName")
@@ -7,7 +6,8 @@ for(var i = 0; i < gameWrapper.length; i++){
 		var ele = document.createElement("span")
 		ele.innerHTML = championImage[j].innerHTML
 		ele.style.position = "absolute"
-//		ele.style.opacity = "0.0"
+		ele.style.fontSize = "8px"
+		ele.style.color = "rgba(0,0,0,0)"
 		summonerName[j].appendChild(ele)
 		championImage[j].innerHTML = ""
 	}
